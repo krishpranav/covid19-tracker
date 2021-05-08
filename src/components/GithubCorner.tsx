bca0b35922794355f9c9c164d16eb3a7fdb87c5d
krishpranav <krisna.pranav@gmail.com>
2021-05-08 11:48:41 +0530

    github corner tsx

---
+++ src/components/GithubCorner.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import { ReactComponent as GithubLogo } from '../assets/github.svg'
+import '../styles/github-corner.css'
+
+const GithubCorner: FC = () => {
+    return (
+        <a
+            href="https://github.com/krishpranav/covid19-tracker"
+            className="fixed top-0 right-0 hidden github-corner md:inline-block"
+            aria-label="View source on GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <GithubLogo />
+        </a>
+    )
+}
+
+export default GithubCorner
