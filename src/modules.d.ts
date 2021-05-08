6c6bd6f45ebd297fc74551f18d7cc49ea1c4c417
krishpranav <krisna.pranav@gmail.com>
2021-05-08 11:32:36 +0530

    update react-app-env

---
+++ src/modules.d.ts
@@ -0,0 +1,5 @@
+// declare modules
+declare module 'world-map-country-shapes'
+declare module 'hammerjs'
+declare module 'frappe-charts/dist/frappe-charts.esm'
+declare module 'tailwindcss/defaultTheme'
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,1 @@
+// <reference types="rect-scripts" />
