6521308a10950f0ee59a5d4985933fc64f6c6164
krishpranav <krisna.pranav@gmail.com>
2021-05-08 11:55:23 +0530

    added transilator

---
+++ src/locales/index.js
@@ -0,0 +1,183 @@
+
+export default {
+    en: {
+      name: 'English',
+      translation: {
+        title: 'COVID-19 stats',
+        confirmed: 'Confirmed',
+        recovered: 'Recovered',
+        deaths: 'Deaths',
+        'last-updated': 'Last updated',
+        'provided-by': 'Provided by',
+        'powered-by': 'Powered by',
+        'change-theme': 'Change theme',
+        'reset-position': 'Reset position',
+        'automatic-manual-zoom': 'Automatic/manual zoom',
+        'zoom-in': 'Zoom in',
+        'zoom-out': 'Zoom out',
+        'world-data': 'World data',
+        'daily-stats': 'Daily stats',
+        'export-chart': 'Export chart'
+      }
+    },
+    es: {
+      name: 'Español',
+      translation: {
+        title: 'Estadísticas del COVID-19',
+        confirmed: 'Confirmados',
+        recovered: 'Recuperados',
+        deaths: 'Muertes',
+        'last-updated': 'Última actualización',
+        'provided-by': 'Proporcionado por',
+        'powered-by': 'Hecho por',
+        'change-theme': 'Cambiar tema',
+        'reset-position': 'Posición inicial',
+        'automatic-manual-zoom': 'Zoom automático/manual',
+        'zoom-in': 'Acercar',
+        'zoom-out': 'Alejar',
+        'world-data': 'Datos mundiales',
+        'daily-stats': 'Estadísticas diarias',
+        'export-chart': 'Exportar gráfico'
+      }
+    },
+    pt: {
+      name: 'Português',
+      translation: {
+        title: 'Estatísticas do COVID-19',
+        confirmed: 'Confirmado',
+        recovered: 'Recuperado',
+        deaths: 'Mortes',
+        'last-updated': 'Ultima atualização',
+        'provided-by': 'Fornecido por',
+        'powered-by': 'Feito por',
+        'change-theme': 'Mudar tema',
+        'reset-position': 'Repor posição',
+        'automatic-manual-zoom': 'Zoom automático/manual',
+        'zoom-in': 'Mais zoom',
+        'zoom-out': 'Reduzir o zoom',
+        'world-data': 'Dados mundiais',
+        'daily-stats': 'Estatísticas diárias',
+        'export-chart': 'Exportar gráfico'
+      }
+    },
+    it: {
+      name: 'Italiano',
+      translation: {
+        title: 'Statistiche COVID-19',
+        confirmed: 'Confermato',
+        recovered: 'Recuperato',
+        deaths: 'Morti',
+        'last-updated': 'Ultimo aggiornamiento',
+        'provided-by': 'Fornito da',
+        'powered-by': 'Fatto da',
+        'change-theme': 'Cambia tema',
+        'reset-position': 'Ripristina posizione',
+        'automatic-manual-zoom': 'Zoom automatico/manuale',
+        'zoom-in': 'Ingrandire',
+        'zoom-out': 'Rimpicciolisci',
+        'world-data': 'Dati mondiali',
+        'daily-stats': 'Statistiche giornaliere',
+        'export-chart': 'Esporta grafico  '
+      }
+    },
+    fr: {
+      name: 'Français',
+      translation: {
+        title: 'Statistiques COVID-19',
+        confirmed: 'Confirmé',
+        recovered: 'Rétabli',
+        deaths: 'Morts',
+        'last-updated': 'Dernière mise à jour',
+        'provided-by': 'Fourni par',
+        'powered-by': 'Faite par',
+        'change-theme': 'Change le thème',
+        'reset-position': 'Réinitialiser la position',
+        'automatic-manual-zoom': 'Zoom automatique/manuel',
+        'zoom-in': 'Agrandir',
+        'zoom-out': 'Dézoomer',
+        'world-data': 'Données mondiales',
+        'daily-stats': 'Statistiques quotidiennes',
+        'export-chart': 'Exporter le graphique'
+      }
+    },
+    ch: {
+      name: '中文',
+      translation: {
+        title: 'COVID-19统计',
+        confirmed: '已确认',
+        recovered: '已恢复',
+        deaths: '死亡人数',
+        'last-updated': '最近更新时间',
+        'provided-by': '由...提供',
+        'powered-by': '由制成',
+        'change-theme': '改变主题',
+        'reset-position': '重置位置',
+        'automatic-manual-zoom': '自动/手动变焦',
+        'zoom-in': '放大',
+        'zoom-out': '缩小',
+        'world-data': '世界数据',
+        'daily-stats': '每日统计',
+        'export-chart': '出口图表'
+      }
+    },
+    gr: {
+      name: 'Ελληνικά',
+      translation: {
+        title: 'COVID-19 στατιστικά στοιχεία',
+        confirmed: 'Επιβεβαιωμένος',
+        recovered: 'Ανακτηθεί',
+        deaths: 'Θάνατοι',
+        'last-updated': 'Τελευταία ενημέρωση',
+        'provided-by': 'Παρέχεται από',
+        'powered-by': 'Φτιαγμένο από',
+        'change-theme': 'Αλλαξε θέμα',
+        'reset-position': 'Επαναφορά θέσης',
+        'automatic-manual-zoom': 'Αυτόματο/χειροκίνητο ζουμ',
+        'zoom-in': 'Μεγέθυνση',
+        'zoom-out': 'Σμίκρυνση',
+        'world-data': 'Παγκόσμια δεδομένα',
+        'daily-stats': 'Καθημερινά στατιστικά στοιχεία',
+        'export-chart': 'Χάρτης εξαγωγών'
+      }
+    },
+    ru: {
+      name: 'Pусский',
+      translation: {
+        title: 'COVID-19 статистика',
+        confirmed: 'Подтвержденные случаи',
+        recovered: 'Выздоровевшие',
+        deaths: 'Смертность',
+        'last-updated': 'Последнее обновление',
+        'provided-by': 'Предоставлено',
+        'powered-by': 'Сделанно в',
+        'change-theme': 'Поменять тематику',
+        'reset-position': 'Сбросить позицию',
+        'automatic-manual-zoom': 'Автоматическое/ручное увеличение',
+        'zoom-in': 'Приблизить',
+        'zoom-out': 'Уменьшить',
+        'world-data': 'Глобально',
+        'daily-stats': 'Ежедневная статистика',
+        'export-chart': 'Экспорт диаграммы'
+      }
+    },
+    vn: {
+      name: 'Tiếng Việt',
+      translation: {
+        title: 'COVID-19 số liệu thống kê',
+        confirmed: 'Xác nhận',
+        recovered: 'Phục hồi',
+        deaths: 'Tử vong',
+        'last-updated': 'Cập nhật mới nhất',
+        'provided-by': 'Cung cấp bởi',
+        'powered-by': 'Được làm bởi',
+        'change-theme': 'Thay đổi nền',
+        'reset-position': 'Về vị trí ban đầu',
+        'automatic-manual-zoom': 'Phóng to/thu nhỏ tự động/bằng tay',
+        'zoom-in': 'Phóng to',
+        'zoom-out': 'Thu nhỏ',
+        'world-data': 'Toàn cầu',
+        'daily-stats': 'Thống kê hàng ngày',
+        'export-chart': 'Biểu đồ xuất khẩu'
+      }
+    }
+  }
