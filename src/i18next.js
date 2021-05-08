e4fc9eaa3489c6cea0635188b48b71c9beeaf173
krishpranav <krisna.pranav@gmail.com>
2021-05-08 11:25:20 +0530

    i 18 next js

---
+++ src/i18next.js
@@ -0,0 +1,18 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import locales from './locales'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: locales,
+    fallbackLng: 'en',
+    debug: process.env.NODE_ENV === 'production' ? false : true,
+    interpolation: {
+      escapeValue: false,
+    }
+});
+
+export default i18n;
